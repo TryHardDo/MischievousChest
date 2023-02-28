81212cc295eda64b8a004d510379079726c4a564
Lévai Levente <levai.levente2002@gmail.com>
2023-02-28 18:06:41 +0100

    Setting up ListenerClass.java & registering in Bukkit

---
+++ src/main/java/dev/tf2levi/mischievouschest/ListenerClass.java
@@ -0,0 +1,11 @@
+package dev.tf2levi.mischievouschest;
+
+import org.bukkit.event.Listener;
+
+public class ListenerClass implements Listener {
+    private final MischievousChest mischievousChest;
+
+    public ListenerClass(MischievousChest mischievousChest) {
+        this.mischievousChest = mischievousChest;
+    }
+}
--- src/main/java/dev/tf2levi/mischievouschest/MischievousChest.java
+++ src/main/java/dev/tf2levi/mischievouschest/MischievousChest.java
@@ -1,17 +1,21 @@
 package dev.tf2levi.mischievouschest;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MischievousChest extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        initPlugin();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void initPlugin() {
+        Bukkit.getPluginManager().registerEvents(new ListenerClass(this), this);
     }
 }
